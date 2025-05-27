c0868f11e2576a7f2f1140303c16f7ae5964d15d
Giabran Mikail <gibranmikail.official@gmail.com>
2025-05-27 15:25:04 +0700

    Update
    Education, Services, Testimonial

--- script.js
+++ script.js
@@ -1,66 +1,61 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-  sections.forEach(sec => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
+    sections.forEach((sec) => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
 
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach(links => {
-        links.classList.remove('active');
-      });
-      document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
-    }
-  });
-}
-
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach((links) => {
+                links.classList.remove("active");
+            });
+            document.querySelector('header nav a[href*="' + id + '"]').classList.add("active");
+        }
+    });
+};
 
 //Menu icon
 menuIcon.onclick = () => {
-  menuIcon.classList.toggle('bx-x');
-  navbar.classList.toggle('active');
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
 };
-
 
 //clear from contact before unload
 window.onbeforeunload = () => {
-  for (const form of document.getElementsByTagName("form")) {
-    form.reset();
-  }
-}
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
+    }
+};
 
 $(document).ready(function () {
-  loadPorto()
+    loadPorto();
 
-  function loadPorto() {
+    function loadPorto() {
+        fetch("data/porto.json")
+            .then((res) => res.json())
+            .then((res) => {
+                let html = "";
+                const data = res.data;
 
-    fetch('data/porto.json')
-      .then((res) => res.json())
-      .then((res) => {
-        let html = ""
-        const data = res.data
-        
-        data?.forEach((el, index) => {
-          
-          
-        let aos = index % 3 
-        // console.log(aos);
+                data?.forEach((el, index) => {
+                    let aos = index % 3;
+                    // console.log(aos);
 
-        let dataAos = ""
-        if(aos == 0){
-          dataAos = "fade-up-right"
-        }else if(aos == 1){
-          dataAos ="fade-up"
-        }else if(aos == 2){
-          dataAos = "fade-up-left"
-        }
-          // slug=gibran-mikail-website-portofolio#contact
-          html += `
+                    let dataAos = "";
+                    if (aos == 0) {
+                        dataAos = "fade-up-right";
+                    } else if (aos == 1) {
+                        dataAos = "fade-up";
+                    } else if (aos == 2) {
+                        dataAos = "fade-up-left";
+                    }
+                    // slug=gibran-mikail-website-portofolio#contact
+                    html += `
             <div data-aos="${dataAos}" data-aos-duration="1000">
                 <div class="grid-item">
                     <img src="${el.primaryImg}" alt="Project 1">
@@ -77,29 +72,23 @@ $(document).ready(function () {
                         <p>${el?.description}</p>
                     </div>
                 </div>
-            </div>`
-        });
-        $("#list-porto").html(html)
-      })
+            </div>`;
+                });
+                $("#list-porto").html(html);
+            });
 
-
-
-      
         // testimoni
-        let htmlTestimoni = ""
-        fetch('data/testimoni.json')
-        .then((res) => res.json())
-        .then((res)=>{
-            res.data.forEach((d)=>{
-              
-              let star = ""
-              for (let i= 0; i < d.star; i++) {
-                star += `<i class='bx bxs-star' id="star"></i>`
-              }
-              
+        let htmlTestimoni = "";
+        fetch("data/testimoni.json")
+            .then((res) => res.json())
+            .then((res) => {
+                res.data.forEach((d) => {
+                    let star = "";
+                    for (let i = 0; i < d.star; i++) {
+                        star += `<i class='bx bxs-star' id="star"></i>`;
+                    }
 
-
-              htmlTestimoni += ` <div data-aos="zoom-in-up" data-aos-duration="500">
+                    htmlTestimoni += ` <div data-aos="zoom-in-up" data-aos-duration="500">
               <div class="testimonial-item">
                   <img src="${d.image}" alt="">
                   <h2>Adzra Fatikha W.</h2>
@@ -109,85 +98,66 @@ $(document).ready(function () {
                       juga kalo tiba-tiba di mintain tolong soal kerjaan dari bos atau gue hehe. hobinya becanda,
                       jadi kalau dikantor gaada ini orang jadi sepi abisss "</p>
               </div>
-          </div> `
-            })
-            $("#testimoni").html(htmlTestimoni)            
-        })
+          </div> `;
+                });
+                $("#testimoni").html(htmlTestimoni);
+            });
 
+        // services
+        let HTMLservices = "";
+        fetch("data/services.json")
+            .then((res) => res.json())
+            .then((res) => {
+                res.data.forEach((d, index) => {
+                    let aos = index % 2;
 
-          // services
-          let HTMLservices = ""
-          fetch('data/services.json')
-          .then((res) => res.json())
-          .then((res)=>{
-            
-            res.data.forEach((d, index)=>{ 
-              
-                 
-        let aos = index % 2
-      
-        // console.log(aos);
+                    // console.log(aos);
 
-        let dataAos = ""
-        if(aos == 0){
-          dataAos = "fade-left"
-        }else if(aos == 1){
-          dataAos ="fade-right"
-        }
-  
-                HTMLservices += ` <div data-aos="${dataAos}" data-aos-duration="1000">
+                    let dataAos = "";
+                    if (aos == 0) {
+                        dataAos = "fade-left";
+                    } else if (aos == 1) {
+                        dataAos = "fade-right";
+                    }
+
+                    HTMLservices += ` <div data-aos="${dataAos}" data-aos-duration="1000">
                 <div class="service-box">
                     <div class="service-info">
-                        <h4>UI/UX Design</h4>
-                        <p>I create visually stunning and user-friendly designs that enhance user experience.
-                            Our approach focuses on intuitive navigation, responsive layouts, and modern aesthetics to
-                            ensure your product not only looks great but also provides seamless interaction.</p>
+                        <h4>${d.title}</h4>
+                        <p>${d.description}</p>
                     </div>
                 </div>
-            </div>`
-              })
-              $("#services-id").html(HTMLservices)            
-          })
-  
-
-
-          
-            // services
-            let HTMLWorkExperience = ""
-            fetch('data/work_experience.json')
-            .then((res) => res.json())
-            .then((res)=>{
-              
-              res.data.forEach((d, index)=>{ 
-                   
-                // console.log();
-                let year = d.date?.split('-');
-
-                let listHTML = ''
-                d.list.forEach(x=> {
-                  listHTML+= `<p>✦ ${x}</p>`
+            </div>`;
                 });
+                $("#services-id").html(HTMLservices);
+            });
 
-                console.log(year);
+        // services
+        let HTMLWorkExperience = "";
+        fetch("data/work_experience.json")
+            .then((res) => res.json())
+            .then((res) => {
+                res.data.forEach((d, index) => {
+                    // console.log();
+                    let year = d.date?.split("-");
 
-                
-          HTMLWorkExperience += `<div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
+                    let listHTML = "";
+                    d.list.forEach((x) => {
+                        listHTML += `<p>✦ ${x}</p>`;
+                    });
+
+                    console.log(year);
+
+                    HTMLWorkExperience += `<div class="timeline-item" data-aos="fade-up" data-aos-duration="1000">
                 <div class="timeline-dot"></div>
                 <div class="timeline-date">${year[0]} - <span>${year[1]}</span></div>
                 <div class="timeline-content">
                     <h3>${d.title} </h3>
                     ${listHTML}
                 </div>
-            </div>`
-                })
-                $("#work_experience").html(HTMLWorkExperience)            
-            })
-    
-  
-          
-  }
-
-
-
-
-})
+            </div>`;
+                });
+                $("#work_experience").html(HTMLWorkExperience);
+            });
+    }
+});
